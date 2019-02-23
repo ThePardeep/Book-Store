55842b28e71817212fc3a8800d9babb014d4fb7a
ThePardeep <pardeepsaini7927@gmail.com>
2019-02-23 13:32:35 +0530

    Final Commit

--- Config/Keys.js
+++ Config/Keys.js
@@ -1,6 +1,6 @@
 const Key = {
-  MongoUrl : process.env.MongoUrl,
-  Secret : process.env.Secret
-}
+  MongoUrl: "mongodb://cbs:StoreBook72@ds137605.mlab.com:37605/ckbookstore",
+  Secret: "CkSecret"
+};
 
 module.exports = Key;
